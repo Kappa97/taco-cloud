9ed74a8676be6364f962b6c0ac0d2b7f0927ecf5
Kappa97 <ionlefter21@gmail.com>
2022-12-19 17:12:32 +0300

    add modified files

--- src/main/java/org/crystal/tacocloud/tacos/data/OrderRepository.java
+++ src/main/java/org/crystal/tacocloud/tacos/data/OrderRepository.java
@@ -1,7 +1,15 @@
 package org.crystal.tacocloud.tacos.data;
 
 import org.crystal.tacocloud.tacos.Order;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
-    Order save(Order order);
+import java.util.Date;
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+    //Order save(Order order);
+    List<Order> findByZip(String deliveryZip);
+    List<Order> readOrdersByZipAndPlacedAtBetween(
+            String deliveryZip, Date startDate, Date endDate);
 }
--- src/main/java/org/crystal/tacocloud/tacos/data/TacoRepository.java
+++ src/main/java/org/crystal/tacocloud/tacos/data/TacoRepository.java
@@ -1,8 +1,7 @@
 package org.crystal.tacocloud.tacos.data;
 
 import org.crystal.tacocloud.tacos.Taco;
+import org.springframework.data.repository.CrudRepository;
 
-public interface TacoRepository {
-
-    Taco save(Taco design);
+public interface TacoRepository extends CrudRepository<Taco, Long> {
 }
